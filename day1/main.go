030ef250bc6b6f74e7e5759f3811acd51bdb53ad
Tom Cotton <cottonthomas.dev@gmail.com>
2025-12-01 22:51:46 +0000

    Day 1 Task 2 better

--- day1/main.go
+++ day1/main.go
@@ -8,62 +8,36 @@ import (
 )
 
 func main() {
-	input := readInput()
+	inputs := readInput()
+	numberOfTimesPassedZeros := 0
+	startingPoint := 50
 
-	var linkedList []*DoubleLLNode
-	firstNode := DoubleLLNode{0, nil, nil}
-	lastNode := DoubleLLNode{99, nil, nil}
-	lastNode.next = &firstNode
-	firstNode.previous = &lastNode
-
-	linkedList = append(linkedList, &firstNode)
-	for i := 1; i <= 98; i++ {
-		if i == 1 {
-			node := DoubleLLNode{i, &firstNode, nil}
-			firstNode.next = &node
-			linkedList = append(linkedList, &node)
-		} else if i == 98 {
-			node := DoubleLLNode{i, linkedList[i-1], &lastNode}
-			lastNode.previous = &node
-			linkedList[i-1].next = &node
-			linkedList = append(linkedList, &node)
-		} else {
-			node := DoubleLLNode{i, linkedList[i-1], nil}
-			linkedList[i-1].next = &node
-			linkedList = append(linkedList, &node)
+	for _, input := range inputs {
+		var temp int
+		if input.noOfTurns > 100 {
+			numberOfTimesPassedZeros = numberOfTimesPassedZeros + (input.noOfTurns / 100)
+			input.noOfTurns = input.noOfTurns % 100
 		}
+		if input.turnDirection == "R" {
+			temp = modIt(startingPoint+input.noOfTurns, 100)
+			if temp < startingPoint {
+				numberOfTimesPassedZeros++
+			}
+		} else {
+			temp = modIt(startingPoint-input.noOfTurns, 100)
+			if temp > startingPoint {
+				numberOfTimesPassedZeros++
+			}
+		}
+
+		startingPoint = temp
 	}
-	linkedList[98].next = &lastNode
-	linkedList = append(linkedList, &lastNode)
 
-	dialNode := linkedList[50]
-	totalZeros := 0
-
-	for _, i := range input {
-		zeros := 0
-		dialNode, zeros = turnNodes(i.turnDirection, i.noOfTurns, 0, dialNode, true)
-		totalZeros = totalZeros + zeros
-	}
-
-	fmt.Println(totalZeros)
-
+	fmt.Println(numberOfTimesPassedZeros)
 }
 
-func turnNodes(direction string, numberOfTurns int, numberOfZeros int, node *DoubleLLNode, newPass bool) (*DoubleLLNode, int) {
-	newNumberOfZeros := numberOfZeros
-	if node.value == 0 && newPass == false {
-		newNumberOfZeros = newNumberOfZeros + 1
-	}
-
-	if numberOfTurns != 0 {
-		if direction == "R" {
-			node, newNumberOfZeros = turnNodes("R", numberOfTurns-1, newNumberOfZeros, node.next, false)
-		} else {
-			node, newNumberOfZeros = turnNodes("L", numberOfTurns-1, newNumberOfZeros, node.previous, false)
-		}
-	}
-
-	return node, newNumberOfZeros
+func modIt(x, y int) int {
+	return ((x % y) + y) % y
 }
 
 func readInput() []TextInput {
@@ -91,10 +65,4 @@ func readInput() []TextInput {
 type TextInput struct {
 	turnDirection string
 	noOfTurns     int
-}
-
-type DoubleLLNode struct {
-	value    int
-	previous *DoubleLLNode
-	next     *DoubleLLNode
 }
